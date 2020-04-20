a6456a517aceb53b85ab1a2571ef12ce6403117d
MoffatMore <12894381+MoffatMore@users.noreply.github.com>
2020-04-20 21:53:03 +0200

    fixing tender evaluation portal

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -26,6 +26,8 @@
     <link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/css/select2.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datetimepicker/4.17.47/css/bootstrap-datetimepicker.min.css" rel="stylesheet" />
     <link href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.5.1/min/dropzone.min.css" rel="stylesheet" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
 
 
 </head>
@@ -318,16 +320,11 @@
   <!-- Page level custom scripts -->
   <script src="js/demo/chart-area-demo.js"></script>
   <script src="js/demo/chart-pie-demo.js"></script>
+
     <!-- Page level plugins -->
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.9.3/Chart.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous">
-    </script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous">
     </script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous">
-    </script>
-
-
     <!-- Custom scripts for all pages-->
     <script src="{{ asset('js/sb-admin-2.min.js') }}"></script>
     <script src="{{ asset('js/jquery-3.3.1.js') }}"></script>
--- resources/views/procurement/tender-evaluation.blade.php
+++ resources/views/procurement/tender-evaluation.blade.php
@@ -4,76 +4,85 @@
     <nav aria-label="breadcrumb">
         <ol class="breadcrumb">
             <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
-            <li class="breadcrumb-item active" aria-current="page">Evaluate Tenders</li>
+            <li class="breadcrumb-item active" aria-current="page">Evaluate Bidders</li>
         </ol>
     </nav>
 @endsection
 
 @section('content')
+
 <div class="row">
     <!-- Begin Page Content -->
     <div class="container-fluid">
         <!-- DataTales Example -->
-        <div class="card shadow mb-4">
-            <div class="card-header py-3">
-            </div>
-            <div class="card-body">
-                <input type="hidden" name="_token" value="{{ @csrf_token() }}">
-                <div class="table-responsive">
-                    <table id="example2" class="table table-striped table-borderless nowrap" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>#</th>
-                                <th>Tender Name</th>
-                                <th>Reference No</th>
-                                <th>Organisation Name</th>
-                                <th>Documents</th>
-                                <th>Qualification</th>
-                                <th>Status</th>
-                                <th>Action</th>
-                            </tr>
-                        </thead>
+        <div class="accordion" id="accordionExample">
+            <input type="hidden" name="_token" value="{{ @csrf_token() }}">
+            @foreach($tenders as $tender)
+                <div class="card">
+                    <div class="card-header" id="heading{{ $tender->id }}">
+                        <h5 class="mb-0">
+                            <button class="btn btn-link" type="button" data-toggle="collapse" data-target="#collapse{{ $tender->id }}" aria-expanded="true" aria-controls="collapseOne">
+                                Tender: {{ $tender->name }}
+                            </button>
+                        </h5>
+                    </div>
 
-                        <tbody>
-                        @foreach($tenders as $tender)
-                            @foreach($tender->bids as $bid)
-                                @if ($bid->status === 'approved')
+                    <div id="collapse{{ $tender->id }}" class="collapse" aria-labelledby="heading{{ $tender->id }}"
+                         data-parent="#accordionExample">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table id="example" class="table table-striped table-borderless nowrap" style="width:100%">
+                                    <thead>
                                     <tr>
-                                        <td>{{ $tender->id }}</td>
-                                        <td>{{ $tender->name }}</td>
-                                        <td>{{ $tender->reference_no }}</td>
-                                        <td>{{ $bid->user->organisation->name }}</td>
-                                        <td>
-                                            @if ($bid->docs === 0)
-                                                <i class="fa fa-times fa-2x text-danger"></i>
-                                            @else
-                                                <i class="fa fa-check fa-2x text-success" ></i>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if ($bid->qualification === 0)
-                                                <i class="fa fa-times fa-2x text-danger"></i>
-                                            @else
-                                                <i class="fa fa-check fa-2x text-success" ></i>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            <i class="fa fa-check fa-2x text-success" ></i>
-                                        </td>
-                                        <td>
-                                            <a class="btn btn-info btn-sm  text-center text-white" href="">
-                                                <i class="fa fa-award"></i> Evauate</a>
-                                        </td>
+                                        <th>#</th>
+                                        <th>Reference No</th>
+                                        <th>Organisation Name</th>
+                                        <th>Documents</th>
+                                        <th>Qualification</th>
+                                        <th>Status</th>
+                                        <th>Action</th>
                                     </tr>
-                                @endif
-                            @endforeach
-                        @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+                                    </thead>
 
+                                    <tbody>
+                                    @foreach($tender->bids as $bid)
+                                        @if ($bid->status === 'approved')
+                                            <tr>
+                                                <td>{{ $tender->id }}</td>
+                                                <td>{{ $tender->reference_no }}</td>
+                                                <td>{{ $bid->user->organisation->name }}</td>
+                                                <td>
+                                                    @if ($bid->docs === 0)
+                                                        <i class="fa fa-times fa-2x text-danger"></i>
+                                                    @else
+                                                        <i class="fa fa-check fa-2x text-success" ></i>
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    @if ($bid->qualification === 0)
+                                                        <i class="fa fa-times fa-2x text-danger"></i>
+                                                    @else
+                                                        <i class="fa fa-check fa-2x text-success" ></i>
+                                                    @endif
+                                                </td>
+                                                <td>
+                                                    <i class="fa fa-check fa-2x text-success" ></i>
+                                                </td>
+                                                <td>
+                                                    <a class="btn btn-info btn-sm  text-center text-white" href="">
+                                                        <i class="fa fa-award"></i> Evauate</a>
+                                                </td>
+                                            </tr>
+                                        @endif
+                                    @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
     <!-- /.container-fluid -->
 </div>
