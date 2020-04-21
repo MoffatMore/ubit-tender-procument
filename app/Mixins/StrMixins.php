11031abd3952f1d5a2000667fb19da54a88a8e9a
MoffatMore <12894381+MoffatMore@users.noreply.github.com>
2020-04-21 02:25:07 +0200

    fixing reference number

--- app/Mixins/StrMixins.php
+++ app/Mixins/StrMixins.php
@@ -8,9 +8,11 @@
 
     class StrMixins
     {
-        public function referenceNumber(): string
+        public function referenceNumber()
         {
-            $reference = Str::random(8);
-            return Str::upper('TN-' . substr($reference, 0, 3) . '-' . substr($reference, 3) . '-UB');
+            return function () {
+                $reference = Str::random(8);
+                return Str::upper('TN-' . substr($reference, 0, 3) . '-' . substr($reference, 3) . '-UB');
+            };
         }
     }
